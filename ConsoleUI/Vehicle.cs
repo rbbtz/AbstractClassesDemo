62ec11d3244e1f93072f6fa4fa357ae80aebd4ac
rbbtz <robbetz@gmail.com>
2020-09-08 14:42:23 -0400

    lift it up

---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ConsoleUI
+{
+    public abstract class Vehicle
+    {
+        public int Year { get; set; } = 1;
+        public string Make { get; set; } = "Default Make";
+        public string Model { get; set; } = "Default Model";
+
+        public abstract void DriveAbstract(); // stubbed out method (means it has no implementation)
+
+        public virtual void DriveVirtual() // does have implementation and scope - virtual method
+        {
+            Console.WriteLine($"This {GetType().Name} is virtually in drive"); // has implention and scope - virtual method
+        } // has implention and scope -  virtual method
+    }
+
+}
